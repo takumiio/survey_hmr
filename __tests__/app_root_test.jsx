628537fcde0f5ab15532e8c61ef0d56b04acba9e
Takumi Nishimura <takumi@nishimura.io>
2019-05-27 23:11:29 +0900

    Create react hook sample (focus event)

--- __tests__/app_root_test.jsx
+++ __tests__/app_root_test.jsx
@@ -1,6 +1,6 @@
 require("./test_helper.js");
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 import AppRoot from "../src/app_root.jsx";
 
@@ -8,5 +8,12 @@ describe("<AppRoot />", () => {
   it("renders an '.title'", () => {
     const wrapper = shallow(<AppRoot />);
     expect(wrapper.find('.title').length).toBe(1);
+  });
+
+  it("viewd when focus textbox", () => {
+    const wrapper = mount(<AppRoot />);
+    expect(wrapper.find(".input_title").length).toBe(1);
+    const input = wrapper.find(".input_title");
+    expect(input.is(":focus")).toEqual(true)
   });
 });
--- src/app_root.jsx
+++ src/app_root.jsx
@@ -2,27 +2,37 @@
 import { jsx } from '@emotion/core'
 import { hot } from "react-hot-loader";
 
-import React from "react";
+import React, { useEffect } from "react";
 
-const AppRoot = (props) => (
-  <div>
-    <h1 
-      className="title" 
-      css={{ 
-        color: 'hotpink' 
-      }}
-    >
-      { props.val }
-    </h1>
+const AppRoot = (props) => { 
+  const input_ref = React.createRef();
+
+  useEffect( () => {
+    input_ref.current.focus();
+  }, []);
+
+  return (
     <div>
-      <input
-        type="text"
-        onChange={ e => props.change_action(e) }
-        value={ props.val }
-      />
+      <h1 
+        className="title" 
+        css={{ 
+          color: 'hotpink' 
+        }}
+      >
+        { props.val }
+      </h1>
+      <div>
+        <input
+          type="text"
+          onChange={ e => props.change_action(e) }
+          className="input_title"
+          ref={ input_ref }
+          value={ props.val }
+        />
+      </div>
     </div>
-  </div>
-);
+  ) 
+};
 
 export default ( process.env.NODE_ENV === "development" ) ?  hot(module)(AppRoot) : AppRoot;
 
